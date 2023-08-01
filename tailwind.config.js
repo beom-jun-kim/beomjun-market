a91cdccea74c4286a0e94f1c23bd9f4d500de7ff
kimbeomjun <gimb52043@gamil.com>
2023-08-01 16:19:51 +0900

    당근마켓 클론 setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+
+  // npm i @tailwindcss/forms
+  // Tailwind 플러그인을 사용하면 새 스타일을 등록
+  plugins: [require('@tailwindcss/forms')],
+}
