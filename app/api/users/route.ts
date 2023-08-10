88d878fd5be689166ce0b5a0b748a45fd3b65716
kimbeomjun <gimb52043@gamil.com>
2023-08-10 17:47:27 +0900

    import 구문 수정

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -18,8 +18,8 @@
 // export default withHandler("POST", POST);
 
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "../../libs/server/withHandler";
-import client from "../../libs/server/client";
+import client from "@/app/libs/server/client";
+import withHandler from "@/app/libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
