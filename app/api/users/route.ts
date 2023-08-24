29fa2ded61daac56a5ce5714a54cba75e655cfc6
kimbeomjun <gimb52043@gamil.com>
2023-08-24 14:21:57 +0900

    ResponseType 추가

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,15 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import {withHandler} from "@/app/libs/server/withHandler";
+import { withHandler, ResponseType } from "@/app/libs/server/withHandler";
 import client from "@/app/libs/server/client";
 import smtpTransport from "@/app/libs/server/email";
 
-export async function POST (req: NextApiRequest, res:NextApiResponse) {
+export async function POST(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { email, phone } = await req.body;
+  console.log("email",email);
+  console.log("phone",phone);
 
   const user = phone ? { phone: +phone } : { email };
   const payload =
     Math.floor(10000 + Math.random() * 90000) + ""; /* + "" : 문자열로 변환 */
   const token = await client.token.create({
+
     // data: ctrl +클릭 , 값에 user가 꼭 필요하다고 나온다(TokenCreateInput)
     data: {
       payload,
@@ -18,11 +24,7 @@ export async function POST (req: NextApiRequest, res:NextApiResponse) {
       user: {
         // connectOrCreate : user가 있을 경우 token연결. 없으면 생성 후 연결
         connectOrCreate: {
-          where: {
-            // 데이터가 존재하는지 확인, 존재하면 update 실행
-
-            ...user,
-          },
+          where: {...user,},
 
           // 데이터가 존재하지 않으면 생성
           create: {
@@ -62,7 +64,7 @@ export async function POST (req: NextApiRequest, res:NextApiResponse) {
   console.log(token);
 
   // NextResponse.json는 첫번째 인자로 응답 데이터 객체를 받아 객체 형식으로 전달
-  return res.status(200).end();
-};
+  return res.status(200).json({ ok: true });
+}
 
 export { withHandler };
