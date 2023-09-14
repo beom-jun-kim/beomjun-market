acf9d26ffee81a7587acfcf70825069ebbea6dfe
kimbeomjun <gimb520432gmai.com>
2023-09-15 05:32:04 +0900

    cloudflare

---
+++ app/api/files/route.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { withHandler, ResponseType } from "@/app/libs/server/withHandler";
+import client from "@/app/libs/server/client";
+import { withApiSession } from "@/app/libs/server/withSession";
+
+export async function GET(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const response = await (
+    await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ID}/images/v2/direct_upload`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.CF_TOKEN}`,
+        },
+      }
+    )
+  ).json();
+
+  return res.json({ ok: true, ...response.result });
+}
+
+export const getProfileRoute = withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler: GET,
+  })
+);
