29003af2442f9ab43bb8f93acdd84885c552df50
kimbeomjun <gimb52043@gamil.com>
2023-09-21 15:52:28 +0900

    next/image

--- app/items/[id]/page.tsx
+++ app/items/[id]/page.tsx
@@ -8,6 +8,7 @@ import { Product, User } from "@prisma/client";
 import useMutation from "@/app/libs/client/useMutation";
 import { cls } from "@/app/libs/client/utils";
 import useUser from "@/app/libs/client/useUser";
+import Image from "next/image";
 
 interface ProductWithUser extends Product {
   user: User;
@@ -45,7 +46,9 @@ const ItemDetail: NextPage = () => {
             className="h-96 bg-slate-300"
           />
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
-            <img
+            <Image
+              width={48}
+              height={48}
               src={`https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${data?.product?.user?.avatar}/avatar`}
               className="w-12 h-12 rounded-full bg-slate-300"
             />
